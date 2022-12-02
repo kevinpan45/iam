8386847cb16a75f08c866f68487f7fa057d44fea
kevinpan45 <kevinpan45@163.com>
2022-12-02 23:03:57 +0800

    init commit

---
+++ src/main/java/io/kp45/security/jose/KeyGeneratorUtils.java
@@ -0,0 +1,35 @@
+package io.kp45.security.jose;
+
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+
+final class KeyGeneratorUtils {
+
+    private KeyGeneratorUtils() {
+    }
+
+    static SecretKey generateSecretKey() {
+        SecretKey hmacKey;
+        try {
+            hmacKey = KeyGenerator.getInstance("HmacSha256").generateKey();
+        } catch (Exception ex) {
+            throw new IllegalStateException(ex);
+        }
+        return hmacKey;
+    }
+
+    static KeyPair generateRsaKey() {
+        KeyPair keyPair;
+        try {
+            KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
+            keyPairGenerator.initialize(2048);
+            keyPair = keyPairGenerator.generateKeyPair();
+        } catch (Exception ex) {
+            throw new IllegalStateException(ex);
+        }
+        return keyPair;
+    }
+}
